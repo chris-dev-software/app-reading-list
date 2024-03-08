b7abf82126138de663b9ebb718c1659d1b41667a
Christopher <chris.dev.software@gmail.com>
2024-03-08 10:36:33 -0500

    Terminando la funcionalidad de agregar y quitar libros de la lista de lectura

--- src/App.jsx
+++ src/App.jsx
@@ -30,13 +30,42 @@ function App () {
     })
   }
 
-  const totalBooks = books.length
+  const removeBookFromList = (ISBN) => {
+    const foundBook = [...list].find(({ book }) => book.ISBN === ISBN)
+
+    if (!foundBook) return
+
+    setList(prevList => {
+      const newList = prevList.filter(item => item.book !== foundBook.book)
+      return newList
+    })
+
+    setBooks(prevBooks => {
+      const newBooks = prevBooks.map(item => {
+        if (item.book !== foundBook.book) return item
+        item.book.inList = false
+        return item
+      })
+
+      return newBooks
+    })
+  }
+
+  const totalBooks = [...books].filter(({ book }) => !book.inList).length
+  const totalList = [...list].length
 
   return (
     <div id='app' className='grid grid-cols-12 h-screen p-5 gap-5'>
       <main className='bg-zinc-800 flex flex-col gap-10 col-span-8 rounded-md p-5 overflow-y-auto'>
         <header className='flex flex-col gap-10'>
-          <h1 className='text-5xl font-semibold'>{totalBooks} <span className='text-3xl tracking-wide'>libros disponibles</span></h1>
+          <div>
+            <h1 className='text-5xl font-semibold mb-2'>{totalBooks} <span className='text-3xl tracking-wide'>libros disponibles</span></h1>
+            {
+              totalList > 0 && (
+                <h2 className='text-3xl font-semibold'>{totalList} <span className='text-xl tracking-wide'>en la lista de lectura</span></h2>
+              )
+            }
+          </div>
           <section className='grid grid-cols-2 gap-10'>
             <div className='flex flex-col gap-5'>
               <label className='text-xl font-medium' htmlFor='pages'>Filtrar por páginas:</label>
@@ -52,43 +81,49 @@ function App () {
           </section>
         </header>
 
-        <section id='container-books' className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 overflow-y-auto'>
+        <section id='container-books' className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 auto-rows-[300px] gap-5 overflow-y-auto'>
           {
             books.map(({ book }) => (
-              <article className={`relative ${book.inList ? 'before:absolute before:inset-0 before:bg-black/80 before:rounded-md' : 'pointer-events-auto group'}`} key={book.ISBN}>
-                <img className='object-cover rounded-md w-full h-full' src={book.cover} alt={book.title} />
-                <div className='absolute opacity-0 pointer-events-none group-hover:opacity-100 group-hover:pointer-events-auto transition-all inset-0 bg-black/50 flex items-center justify-center text-center flex-col backdrop-blur-sm gap-2 rounded-md px-2'>
-                  <h2 className='text-xl'>{book.title}</h2>
-                  <button onClick={() => addBookToList(book.ISBN)} className='bg-zinc-900 flex items-center gap-2 px-4 py-1 rounded-full'>
-                    Mover a la lista de lectura
-                    <RiArrowRightSLine className='text-xl' />
-                  </button>
+              <article className={`relative w-full h-full overflow-hidden rounded-md ${book.inList ? 'before:absolute before:inset-0 before:bg-black/80 before:rounded-md' : 'pointer-events-auto group'}`} key={book.ISBN}>
+                <img className='object-cover w-full h-full' src={book.cover} alt={book.title} />
+                <div className='absolute inset-0 bg-black/80 text-center opacity-0 pointer-events-none group-hover:opacity-100 group-hover:pointer-events-auto transition-all'>
+                  <div className='flex flex-col h-full items-center justify-center p-2 gap-2'>
+                    <h2 className='text-xl'>{book.title}</h2>
+                    <button onClick={() => addBookToList(book.ISBN)} className='bg-zinc-900 flex items-center px-4 py-1 rounded-full'>
+                      Mover
+                      <RiArrowRightSLine className='text-xl' />
+                    </button>
+                  </div>
                 </div>
               </article>
             ))
           }
         </section>
       </main>
-      <aside className='bg-zinc-800 flex flex-col col-span-4 rounded-md p-5 overflow-y-auto'>
-        <h1 className='text-4xl font-semibold'>Lista de lectura</h1>
+      {
+        list.length > 0 && (
+          <aside className='bg-zinc-800 flex flex-col gap-10 col-span-4 rounded-md p-5 overflow-y-auto'>
+            <h1 className='text-4xl font-semibold'>Lista de lectura</h1>
 
-        <section id='container-books' className='grid grid-cols-1 sm:grid-cols-2  gap-10 overflow-y-auto'>
-          {
+            <section id='container-books' className='grid grid-cols-1 lg:grid-cols-2 gap-5 overflow-y-auto'>
+              {
             list.map(({ book }) => (
               <article className='relative group' key={book.ISBN}>
                 <img className='object-cover rounded-md w-full h-full' src={book.cover} alt={book.title} />
                 <div className='absolute opacity-0 pointer-events-none group-hover:opacity-100 group-hover:pointer-events-auto transition-all inset-0 bg-black/50 flex items-center justify-center text-center flex-col backdrop-blur-sm gap-2 rounded-md px-2'>
                   <h2 className='text-xl'>{book.title}</h2>
-                  <button className='bg-zinc-900 flex items-center gap-2 px-4 py-1 rounded-full'>
-                    Mover a la lista de disponibles
+                  <button onClick={() => removeBookFromList(book.ISBN)} className='bg-zinc-900 flex items-center gap-2 px-4 py-1 rounded-full'>
+                    Retirar
                     <RiArrowRightSLine className='text-xl' />
                   </button>
                 </div>
               </article>
             ))
           }
-        </section>
-      </aside>
+            </section>
+          </aside>
+        )
+      }
     </div>
   )
 }
