03f16e9ec48f9f43eec7f5fe2ba444c6b5a279f8
Christopher <chris.dev.software@gmail.com>
2024-03-08 16:08:44 -0500

    Separando los estados en customHooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import data from './libs/books.json'
 import { RiArrowRightSLine } from 'react-icons/ri'
 
-function App () {
-  const [books, setBooks] = useState([])
-  const [list, setList] = useState([])
-  const [filters, setFilters] = useState({
-    max: 0,
-    genre: 'all'
-  })
-
-  useEffect(() => {
-    setBooks(data.library)
-  }, [])
+const useBooks = (initialBooks) => {
+  const [books, setBooks] = useState(initialBooks)
+  const [bookList, setBookList] = useState([])
 
   const addBookToList = (ISBN) => {
     const foundBook = [...books].find(({ book }) => book.ISBN === ISBN)
     if (!foundBook) return
 
-    setList(prevList => {
+    setBookList(prevList => {
       const newList = [...prevList, foundBook]
       return newList
     })
@@ -34,22 +26,12 @@ function App () {
     })
   }
 
-  const handleChangeRange = (event) => {
-    const newRange = Number(event.target.value)
-    setFilters({ ...filters, max: newRange })
-  }
-
-  const handleChangeSelect = (event) => {
-    const newGenre = event.target.value
-    setFilters({ ...filters, genre: newGenre })
-  }
-
   const removeBookFromList = (ISBN) => {
-    const foundBook = [...list].find(({ book }) => book.ISBN === ISBN)
+    const foundBook = [...bookList].find(({ book }) => book.ISBN === ISBN)
 
     if (!foundBook) return
 
-    setList(prevList => {
+    setBookList(prevList => {
       const newList = prevList.filter(item => item.book !== foundBook.book)
       return newList
     })
@@ -65,29 +47,69 @@ function App () {
     })
   }
 
+  const totalBookList = [...bookList].length
+
+  return { books, bookList, removeBookFromList, addBookToList, totalBookList }
+}
+
+const useFilters = (books) => {
+  const [filters, setFilters] = useState({
+    max: 0,
+    genre: 'all'
+  })
+
+  const handleChangeRange = (event) => {
+    const newRange = Number(event.target.value)
+    setFilters({ ...filters, max: newRange })
+  }
+
+  const handleChangeSelect = (event) => {
+    const newGenre = event.target.value
+    setFilters({ ...filters, genre: newGenre })
+  }
+
   const filteredBooks = [...books].filter(({ book }) => {
     return (book.pages > filters.max && (book.genre === filters.genre || filters.genre === 'all'))
   })
 
-  const totalBooks = [...filteredBooks].filter(({ book }) => !book.inList).length
-  const totalList = [...list].length
+  const totalAvailableBooks = [...filteredBooks].filter(({ book }) => !book.inList).length
+
+  return { filteredBooks, handleChangeRange, handleChangeSelect, filters, totalAvailableBooks }
+}
+
+function App () {
+  const {
+    books,
+    bookList,
+    removeBookFromList,
+    addBookToList,
+    totalBookList
+  } = useBooks(data.library)
+
+  const {
+    filteredBooks,
+    filters,
+    handleChangeRange,
+    handleChangeSelect,
+    totalAvailableBooks
+  } = useFilters(books)
 
   return (
     <div id='app' className='grid grid-cols-12 h-screen p-5 gap-5'>
       <main className='bg-zinc-800 flex flex-col gap-10 col-span-8 rounded-md p-5 overflow-y-auto'>
         <header className='flex flex-col gap-10'>
           <div>
-            <h1 className='text-5xl font-semibold mb-2'>{totalBooks} <span className='text-3xl tracking-wide'>libros disponibles</span></h1>
+            <h1 className='text-5xl font-semibold mb-2'>{totalAvailableBooks} <span className='text-3xl tracking-wide'>libros disponibles</span></h1>
             {
-              totalList > 0 && (
-                <h2 className='text-3xl font-semibold'>{totalList} <span className='text-xl tracking-wide'>en la lista de lectura</span></h2>
+              totalBookList > 0 && (
+                <h2 className='text-3xl font-semibold'>{totalBookList} <span className='text-xl tracking-wide'>en la lista de lectura</span></h2>
               )
             }
           </div>
           <section className='grid grid-cols-2 gap-10'>
             <div className='flex flex-col gap-5'>
               <label className='text-xl font-medium' htmlFor='pages'>Filtrar por páginas:</label>
-              <input onChange={handleChangeRange} min={0} max={1000} value={filters.max} id='pages' type='range' />
+              <input onChange={handleChangeRange} min={0} max={700} value={filters.max} id='pages' type='range' />
             </div>
 
             <div className='flex flex-col gap-5'>
@@ -123,13 +145,13 @@ function App () {
         </section>
       </main>
       {
-        list.length > 0 && (
+        bookList.length > 0 && (
           <aside className='bg-zinc-800 flex flex-col gap-10 col-span-4 rounded-md p-5 overflow-y-auto'>
             <h1 className='text-4xl font-semibold'>Lista de lectura</h1>
 
             <section id='container-books' className='grid grid-cols-1 lg:grid-cols-2 gap-5 overflow-y-auto'>
               {
-            list.map(({ book }) => (
+            bookList.map(({ book }) => (
               <article className='relative group' key={book.ISBN}>
                 <img className='object-cover rounded-md w-full h-full' src={book.cover} alt={book.title} />
                 <div className='absolute opacity-0 pointer-events-none group-hover:opacity-100 group-hover:pointer-events-auto transition-all inset-0 bg-black/50 flex items-center justify-center text-center flex-col backdrop-blur-sm gap-2 rounded-md px-2'>
